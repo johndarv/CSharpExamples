032d4fa6a8852b90730bbee4cc40aea46ac93dbc
JohnDarv <JohnDarv@Hotmail.com>
2014-05-24 14:21:15 +0100

    Lock example and Mutex example projects added. Need to refactor Mutex one a bit.

---
+++ JohnDarv.CSharp.Examples.Lock/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JohnDarv.CSharp.Examples.Lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<ThingWithUniqueId> things = new List<ThingWithUniqueId>()
+            {
+                new ThingWithUniqueId(10),
+                new ThingWithUniqueId(55)
+            };
+
+            IList<Thread> threads = new List<Thread>();
+
+            SharedResourceWithoutLock withoutLock = new SharedResourceWithoutLock(0);
+            SharedResourceWithLock withLock = new SharedResourceWithLock(0);
+
+            things.OrderBy(t => t.Id);
+
+            foreach (ThingWithUniqueId thing in things)
+            {
+                threads.Add(new Thread(new ThreadStart(() => withoutLock.UpdateTheNumber(thing.Id))));
+                threads.Add(new Thread(new ThreadStart(() => withLock.UpdateTheNumber(thing.Id))));
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                Console.WriteLine("The number of shared resource *without* the lock is: {0}", withoutLock.GetTheNumber());
+                Console.WriteLine("The number of shared resource *with* the lock is: {0}", withLock.GetTheNumber());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ISharedResource
+    {
+        int GetTheNumber();
+
+        void UpdateTheNumber(int updatedNumber);
+    }
+
+    abstract class BaseSharedResource : ISharedResource
+    {
+        protected  int number;
+
+        protected BaseSharedResource(int initialNumber)
+        {
+            this.number = initialNumber;
+        }
+    
+        public int GetTheNumber()
+        {
+ 	        return this.number;
+        }
+
+        public abstract void UpdateTheNumber(int updatedNumber);
+
+        protected void BaseUpdateTheNumber(int updatedNumber)
+        {
+            Console.WriteLine("Starting to update the number to {0}...", updatedNumber);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            this.number = updatedNumber;
+            Console.WriteLine("Finished updating the number to {0}.", updatedNumber);
+        }
+    }
+
+    class SharedResourceWithoutLock : BaseSharedResource
+    {
+        public SharedResourceWithoutLock(int initialNumber)
+            : base(initialNumber)
+        {
+        }
+
+        public override void UpdateTheNumber(int updatedNumber)
+        {
+            BaseUpdateTheNumber(updatedNumber);
+        }
+    }
+
+    class SharedResourceWithLock : BaseSharedResource
+    {
+        private object lockObject = new Object();
+
+        public SharedResourceWithLock(int initialNumber)
+            : base(initialNumber)
+        {
+        }
+
+        public override void UpdateTheNumber(int updatedNumber)
+        {
+ 	        lock(lockObject)
+            {
+                BaseUpdateTheNumber(updatedNumber);
+            }
+        }
+    }
+
+    class ThingWithUniqueId
+    {
+        public ThingWithUniqueId(int id)
+        {
+            this.Id = id;
+        }
+
+        public readonly int Id;
+    }
+}
---
+++ JohnDarv.CSharp.Examples.MutexProgram/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JohnDarv.CSharp.Examples.MutexProgram
+{
+    class Program
+    {
+        private const string uniqueMutexName = "MutexExample";
+
+        private static Mutex mutex;
+
+        static void Main(string[] args)
+        {
+            if (OnlyMutexExampleProgramRunning())
+            {
+                Console.WriteLine("This is the only Mutex Example Program running on this computer.");
+            }
+            else
+            {
+                Console.WriteLine("There is another Mutex Example Program running on this computer.");
+            }
+
+            Console.WriteLine("Please specify in seconds how long you want to get the mutex signal for...");
+
+            string input = string.Empty;
+
+            while (input != "quit" && input != "q")
+            {
+                input = Console.ReadLine();
+
+                int seconds;
+                bool isNumber = int.TryParse(input, out seconds);
+
+                if (isNumber)
+                {
+                    TryMonopoilzeMutex(TimeSpan.FromSeconds(seconds));
+                }
+            }
+
+            Program.mutex.Close();
+            Program.mutex.Dispose();
+        }
+
+        static bool OnlyMutexExampleProgramRunning()
+        {
+            try
+            {
+                Program.mutex = Mutex.OpenExisting(uniqueMutexName);
+            }
+            catch (Exception ex)
+            {
+                Program.mutex = new Mutex(false, uniqueMutexName);
+
+                // This must be the only instance running
+                return true;
+            }
+
+            // There was at least one other instance running
+            return false;
+        }
+
+        static void TryMonopoilzeMutex(TimeSpan timespan)
+        {
+            Console.WriteLine("Trying to Monopolize Mutex...");
+            bool wasAbleToMonopolize = Program.mutex.WaitOne(0);
+
+            if (wasAbleToMonopolize)
+            {
+                Console.WriteLine("I got a signal! Waiting for {0} seconds...", timespan.Seconds);
+                
+                Thread.Sleep(timespan);
+
+                Console.WriteLine("Releasing Mutex now!");
+                Program.mutex.ReleaseMutex();
+            }
+            else
+            {
+                Console.WriteLine("Another program has the signal for now...");
+            }
+        }
+    }
+}
