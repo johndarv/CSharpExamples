13284f96ec26ed1d526c9dd54bda29a07378a4a6
JohnDarv <JohnDarv@Hotmail.com>
2014-09-15 19:32:45 +0100

    Adding OwinSelfHost example.

---
+++ JohnDarv.CSharp.Examples.OwinSelfHost/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace JohnDarv.CSharp.Examples.OwinSelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{name}",
+                defaults: new { name = RouteParameter.Optional });
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
